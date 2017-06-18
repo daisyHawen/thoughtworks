c6304f7d8970d78051fb74f8f6debcd3ca535407
daisyHawen <512309453@qq.com>
2017-06-18 12:39:05 +0800

    ques001

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
+const mymod = require('./mod.js')
 module.exports = function main() {
     console.log("Debug Info");
+    mymod(9, 4);
+    mymod(9, 3);
     return 'Hello World!';
 };
---
+++ main/mod.js
@@ -0,0 +1,4 @@
+/*求两个数的余数*/
+module.exports = function(a, b) {
+    return a % b;
+}
